4fb571210d7300bd10ea1fae230faf7b14fbb6d0
aaron <aaron@DESKTOP-ME0GL35>
2024-03-21 18:50:18 +0100

    Añadida la documentacion JavaDoc

--- PractivaUT5JavaDoc/src/clase/Daw1.java
+++ PractivaUT5JavaDoc/src/clase/Daw1.java
@@ -2,78 +2,68 @@ package clase;
 
 import java.util.ArrayList;
 
+/**
+ * Clase principal que representa una clase de programación de nivel DAW1.
+ */
 public class Daw1 {
-	public static ArrayList<IntegrantesClase> clase;
+    public static ArrayList<IntegrantesClase> clase;
 
-	public static void main(String[] args) {
-		clase = new ArrayList<>();
+    public static void main(String[] args) {
+        clase = new ArrayList<>();
 
-		clase.add(new Profesor("51230681B", "Lucas", 0, 0, 0, false));
-		clase.add(new Alumno("53456681B", "Aaron", 24, null, 2000));
-		clase.add(new Alumno("51267081B", "Valeria", 14, null, 2010));
+        clase.add(new Profesor("51230681B", "Lucas", 33, 2500, 1, false));
+        clase.add(new Alumno("53456681B", "Aaron", 24, "Carlos", 2000));
+        clase.add(new Alumno("51267081B", "Valeria", 14, "Francisco", 2010));
 
-		clase.add(new Profesor("41256061B", "Miguel", 0, 0, 0, false));
-		clase.add(new Alumno("57898681B", "Julian", 24, null, 2000));
-		clase.add(new Alumno("34789681B", "Sebastian", 16, null, 2008));
+        clase.add(new Profesor("41256061B", "Miguel", 67, 4000, 3, true));
+        clase.add(new Alumno("57898681B", "Julian", 24, "Fernando", 2000));
+        clase.add(new Alumno("34789681B", "Sebastian", 16, "Paco", 2008));
 
-		pasarLista();
-		System.out.println("----------");
-		esMayorEdad();
-		System.out.println("----------");
-		System.out.println("La nota media de la clase es: " + notaMedia());
-		
-		
-
-		
-		
-		
-
-	}
-
-	public static void pasarLista() {
-		for (IntegrantesClase integrantes : clase) {
-			if (integrantes instanceof Alumno) {
-				System.out.println(integrantes);
-
-			}
-
-		}
-
-	}
-
-	public static void esMayorEdad() {
-		for (IntegrantesClase integrantes : clase) {
-			if (integrantes instanceof Alumno) {
-				((Alumno) integrantes).esMenorEdad();
-
-			}
-
-		}
-
-	}
-	
-	public static double notaMedia() {
-		double notaMedia = 0;
-		for (IntegrantesClase alumnos : clase) {
-			if (alumnos instanceof Alumno) {
-				notaMedia = ((Alumno) alumnos).calcularNotaMedia();
-				
-			}
-			
-		}
-		
-		return notaMedia;
-		
-		
-		
-		
-		
-		
-		
-		
-	}
-	
-	
+        pasarLista();
+        System.out.println("----------");
+        esMayorEdad();
+        System.out.println("----------");
+        System.out.println("La nota media de la clase es: " + notaMedia());
 
 
+    }
+
+    /**
+     * Método para pasar lista, imprimiendo los alumnos presentes.
+     */
+    public static void pasarLista() {
+        for (IntegrantesClase integrantes : clase) {
+            if (integrantes instanceof Alumno) {
+                System.out.println(integrantes);
+            }
+        }
+    }
+
+    /**
+     * Método para comprobar si los alumnos son mayores o menores de edad.
+     */
+    public static void esMayorEdad() {
+        for (IntegrantesClase integrantes : clase) {
+            if (integrantes instanceof Alumno) {
+                ((Alumno) integrantes).esMenorEdad();
+            }
+        }
+    }
+
+    /**
+     * Método para calcular la nota media de la clase.
+     *
+     * @return La nota media de los alumnos en la clase.
+     */
+    public static double notaMedia() {
+        double suma = 0;
+        int contador = 0;
+        for (IntegrantesClase integrante : clase) {
+            if (integrante instanceof Alumno) {
+                suma += ((Alumno) integrante).calcularNotaMedia();
+                contador++;
+            }
+        }
+        return suma / contador;
+    }
 }
--- PractivaUT5JavaDoc/src/clase/Profesor.java
+++ PractivaUT5JavaDoc/src/clase/Profesor.java
@@ -1,58 +1,78 @@
 package clase;
 
-import java.util.ArrayList;
-
+/**
+ * Clase que representa a un profesor en una clase.
+ */
 public class Profesor extends IntegrantesClase {
-	private double sueldo;
-	private int numeroHijos;
-	private boolean jefeDepartamento;
+    private double sueldo;
+    private int numeroHijos;
+    private boolean jefeDepartamento;
 
-	public Profesor(String dni, String nombre, int edad, double sueldo, int numeroHijos, boolean jefeDepartamento) {
-		super(dni, nombre, edad);
-		this.sueldo = sueldo;
-		this.numeroHijos = numeroHijos;
-		this.jefeDepartamento = jefeDepartamento;
-	}
+    /**
+     * Constructor de la clase Profesor.
+     *
+     * @param dni               El DNI del profesor.
+     * @param nombre            El nombre del profesor.
+     * @param edad              La edad del profesor.
+     * @param sueldo            El sueldo del profesor.
+     * @param numeroHijos       El número de hijos del profesor.
+     * @param jefeDepartamento  Indica si el profesor es jefe de departamento.
+     */
+    public Profesor(String dni, String nombre, int edad, double sueldo, int numeroHijos, boolean jefeDepartamento) {
+        super(dni, nombre, edad);
+        this.sueldo = sueldo;
+        this.numeroHijos = numeroHijos;
+        this.jefeDepartamento = jefeDepartamento;
+    }
 
-	public double getSueldo() {
-		return sueldo;
-	}
+    public double getSueldo() {
+        return sueldo;
+    }
 
-	public void setSueldo(double sueldo) {
-		this.sueldo = sueldo;
-	}
+    public void setSueldo(double sueldo) {
+        this.sueldo = sueldo;
+    }
 
-	public int getNumeroHijos() {
-		return numeroHijos;
-	}
+    public int getNumeroHijos() {
+        return numeroHijos;
+    }
 
-	public void setNumeroHijos(int numeroHijos) {
-		this.numeroHijos = numeroHijos;
-	}
+    public void setNumeroHijos(int numeroHijos) {
+        this.numeroHijos = numeroHijos;
+    }
 
-	public boolean isJefeDepartamento() {
-		return jefeDepartamento;
-	}
+    public boolean isJefeDepartamento() {
+        return jefeDepartamento;
+    }
 
-	public void setJefeDepartamento(boolean jefeDepartamento) {
-		this.jefeDepartamento = jefeDepartamento;
-	}
+    public void setJefeDepartamento(boolean jefeDepartamento) {
+        this.jefeDepartamento = jefeDepartamento;
+    }
 
-	@Override
-	public String toString() {
-		return "Profesor [dni=" + dni + ", nombre=" + nombre + ", edad=" + edad + ", sueldo=" + sueldo + "]";
-	}
+    /**
+     * Retorna una representación en cadena del profesor.
+     *
+     * @return Una cadena que representa al profesor.
+     */
+    @Override
+    public String toString() {
+        return "Profesor [dni=" + dni + ", nombre=" + nombre + ", edad=" + edad + ", sueldo=" + sueldo + "]";
+    }
 
-	@Override
-	public void imprimirInformacion() {
-		System.out.println(toString());
-	}
+    /**
+     * Método para imprimir la información del profesor.
+     */
+    @Override
+    public void imprimirInformacion() {
+        System.out.println(toString());
+    }
 
-	/**
-	 * Método para comprobar la validez de un DNI.
-	 * 
-	 * @return true si el DNI es válido, false si no lo es.
-	 */
+    /**
+     * Método para comprobar la validez del DNI del profesor.
+     *
+     * @return true si el DNI es válido, false si no lo es.
+     */
+    @Override
 	public boolean comprobarDni() {
 		// 51346081B
 		int contador = 0;
@@ -83,18 +103,18 @@ public class Profesor extends IntegrantesClase {
 		return true;
 	}
 
-	public boolean esJedeDepartamento() {
-		if (this.sueldo < 2000) {
-			setJefeDepartamento(false);
-			return false;
-
-		} else {
-
-			setJefeDepartamento(true);
-			return true;
-
-		}
-
-	}
-
+    /**
+     * Determina si el profesor es jefe de departamento basándose en su sueldo.
+     *
+     * @return true si el profesor es jefe de departamento, false si no lo es.
+     */
+    public boolean esJefeDepartamento() {
+        if (this.sueldo < 2000) {
+            setJefeDepartamento(false);
+            return false;
+        } else {
+            setJefeDepartamento(true);
+            return true;
+        }
+    }
 }
